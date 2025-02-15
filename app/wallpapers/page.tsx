cfd0878deeaad1fa65e232a7fe48b4a81f66292b
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-02-15 10:58:35 +0530

    Added wallpapers

---
+++ app/wallpapers/page.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from "@/components/helpers/NavBar";
+
+export default function Home(){
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+        </div>
+    );
+}
