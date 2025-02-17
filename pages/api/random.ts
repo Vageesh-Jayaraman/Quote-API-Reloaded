862c9b18899c16a05160fea8a40b7acc0560814f
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-02-17 13:21:26 +0530

    Added mongodb connectivity

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -3,22 +3,24 @@ import connectToDatabase from "@/lib/mongodb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const db = await connectToDatabase()
-    if(!db){
+    const db = await connectToDatabase();
+    if (!db) {
       throw new Error("Db not found");
     }
-    const data = await db.collection("quote").find({}).toArray();
+
+    const data = await db.collection("quote").find({}).project({ _id: 0 }).toArray();
     const len = data.length;
+    
     if (len === 0) {
-      res.status(404).json({ error: "No quotes found" });
-      return;
+      return res.status(404).json({ error: "No quotes found" });
     }
 
     const index = Math.floor(Math.random() * len);
     const randomQuote = data[index];
-    res.status(200).json(randomQuote);
+
+    return res.status(200).json(randomQuote);
   } catch (error) {
     console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 }
