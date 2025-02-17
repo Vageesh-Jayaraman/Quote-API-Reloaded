f59843bc682b7f25db2b92c8e11e1bf3a0d5863f
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-02-17 20:26:41 +0530

    Under dev

--- components/helpers/QOA.tsx
+++ components/helpers/QOA.tsx
@@ -10,7 +10,7 @@ export default function QuoteOfTheDay() {
     <div className="mx-20 mt-10">
       <div className="relative p-20 w-50 h-fit hover:border-2 bg-gradient-to-r from-[#006835] to-[#73E568] flex justify-center items-center rounded-md">
         <p className="absolute top-2 left-2 text-base font-mono font-bold bg-black px-2 py-1 rounded-md text-neutral-400">
-          Quote of the Day
+          Still under dev!
         </p>
 
         <p className={`text-center text-4xl text-white  ${roboto.className} p-10`}>
